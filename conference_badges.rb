f746819302a56aa5607b4cde777d2d0ddfed572d
assane savadogo <assane.savadogo81@gmail.com>
2019-07-26 02:41:52 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,5 +9,9 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(speakers)
-  speakers.each_with_index {|name, index| "Hello, #{name}! You'll be assigned to room #{index + 1}!"}
+  new_arr = []
+  speakers.each_with_index do |name, index| 
+    new_arr << "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+  end
+  new_arr
 end
